4e35daeb8885cfa06be543f4598260eb94427401
JackTheGitHub <39993837+JackTheGitHub@users.noreply.github.com>
2019-01-23 02:14:32 +0200

    Save

---
+++ MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using MVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<mvcEmployeeModel> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<mvcEmployeeModel>>().Result;
+            return View(empList);
+        }
+
+        public ActionResult AddorEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new mvcEmployeeModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee/"+id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<mvcEmployeeModel>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddorEdit(mvcEmployeeModel emp)
+        {
+            if (emp.EmployeeID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", emp).Result;
+                TempData["SuccessMessage"] = "Saved Successfuly";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employee/"+emp.EmployeeID, emp).Result;
+                TempData["SuccessMessage"] = "Updated Successfuly";
+            }
+            return RedirectToAction("index");
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employee/"+id.ToString()).Result;  
+            TempData["SuccessMessage"] = "Deleted Successfuly";
+            return RedirectToAction("index");
+        }
+    }
+}
